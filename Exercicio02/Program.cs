ce3dba2fea6e5339f470a57003ef09fb835cc819
Silvio Limeira <sicemal@gmail.com>
2021-03-16 11:31:44 -0300

    1.5.4 - Correção

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            int numero = int.Parse(Console.ReadLine());
+            while (numero > 0)
+            {
+                Console.WriteLine(Math.Sqrt(numero).ToString("F3"));
+                Console.WriteLine("Digite outro número: ");
+                numero = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Número Negativo!");
+
+        }
+    }
+}
