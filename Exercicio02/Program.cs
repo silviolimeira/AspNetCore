3eead29dc81bc87529afb47ba2bc104f8313814d
Silvio Limeira <sicemal@gmail.com>
2021-03-16 11:37:02 -0300

    add Exercicio 02.

--- Exercicio02/Program.cs
+++ Exercicio02/Program.cs
@@ -7,12 +7,12 @@ namespace Exercicio02
         static void Main(string[] args)
         {
             Console.WriteLine("Digite um número: ");
-            int numero = int.Parse(Console.ReadLine());
+            double numero = double.Parse(Console.ReadLine());
             while (numero > 0)
             {
                 Console.WriteLine(Math.Sqrt(numero).ToString("F3"));
-                Console.WriteLine("Digite outro número: ");
-                numero = int.Parse(Console.ReadLine());
+                Console.Write("Digite outro número: ");
+                numero = double.Parse(Console.ReadLine());
             }
             Console.WriteLine("Número Negativo!");
 
